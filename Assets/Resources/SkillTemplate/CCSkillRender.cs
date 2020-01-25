1908a8b04f5b399fb0afaa2dc5c9d55c4127c6fc
Steven Cheng <stv.chm@gmail.com>
2020-01-25 22:32:41 +0800

    update skill Animate

---
+++ Assets/Resources/SkillTemplate/CCSkillRender.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CCSkillRender: MonoBehaviour {
+    public List<GameObject> Skill_preload;
+    public GameObject Backgroud;
+    private Material k;
+    public Color EX_Color { get { return new Color(1f, 1f, 0f, 1f); } }
+    public Color Nor_Color { get { return new Color(1f,0f,0f,1f); } }
+    public float default_height_limit = 500f;
+    void Start(){
+        if (Backgroud == null){
+            Backgroud  = this.transform.Find("background").gameObject;
+        }
+        k = Backgroud.GetComponent<Image>().material;
+        default_height_limit =  Backgroud.GetComponent<RectTransform>().rect.height;
+    }
+    public IEnumerator LoadCCImg(AssetBundle abs , int level) {
+        
+        yield return true;
+    }
+}

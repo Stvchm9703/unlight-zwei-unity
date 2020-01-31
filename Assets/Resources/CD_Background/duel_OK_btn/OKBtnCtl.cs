947c935cef050d34c5e5b13b97504bb94eb7ab5c
steven <stv.chm@gmail.com>
2020-01-31 23:51:12 +0800

    add duel-ok-btn , phase turn display

---
+++ Assets/Resources/CD_Background/duel_OK_btn/OKBtnCtl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+public class OKBtnCtl : MonoBehaviour {
+    // public mainCtl
+    public GameObject Lock;
+
+    public bool is_lock;
+
+    public GameObject click_btn;
+    public Color able_color;
+    public Color disable_color;
+    Material ok_shad;
+    private void Start () {
+        if (Lock == null) {
+            Lock = this.transform.Find ("lock").gameObject;
+        }
+        if (click_btn == null) {
+            click_btn = this.transform.Find ("ok_btn").gameObject;
+        }
+        ok_shad = click_btn.GetComponent<SpriteRenderer> ().material;
+        SetAble (false);
+    }
+    public void OKOnClick () {
+        if (!is_lock) {
+            // do
+        }
+    }
+    public void SetAble (bool able) {
+        if (able) {
+            Lock.SetActive (false);
+            Debug.Log (ok_shad.name);
+            ok_shad.SetColor ("_MaskColor", able_color);
+            ok_shad.SetInt ("_DisableFlash", 0);
+        } else {
+            Lock.SetActive (true);
+            ok_shad.SetColor ("_MaskColor", disable_color);
+            ok_shad.SetInt ("_DisableFlash", 1);
+        }
+
+        is_lock = !able;
+
+    }
+}
+
+#if (UNITY_EDITOR) 
+[CustomEditor (typeof (OKBtnCtl))]
+public class OKBtnCtl_Editor : Editor {
+    public override void OnInspectorGUI () {
+        DrawDefaultInspector ();
+        OKBtnCtl d = (OKBtnCtl) target;
+
+        if (GUILayout.Button ("Test OK on")) {
+            d.SetAble (true);
+        }
+        if (GUILayout.Button ("Test OK off")) {
+            d.SetAble (false);
+        }
+
+    }
+}
+
+#endif
---
+++ Assets/Resources/CD_Background/duel_OK_btn/OkBtnOnClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class OkBtnOnClick : MonoBehaviour {
+    public OKBtnCtl main_ctl;
+    private void Start () {
+        if (main_ctl == null) {
+            main_ctl = this.transform.parent.GetComponent<OKBtnCtl>();
+        }
+    }
+    private void OnMouseDown() {
+        main_ctl.OKOnClick();
+    }
+}

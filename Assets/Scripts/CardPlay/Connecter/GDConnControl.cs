1c9ee41678b5cfd66040b61819885f7aa45ef36e
steven <stv.chm@gmail.com>
2020-05-08 17:47:25 +0800

    split script

--- Assets/Scripts/CardPlay/Connecter/GDConnControl.cs
+++ Assets/Scripts/CardPlay/Connecter/GDConnControl.cs
@@ -13,16 +13,6 @@ using UnityEngine;
 
 public class GDConnControl : Singleton<GDConnControl> {
   // Game other Controller
-  public CCardSetUp cCardResx;
-  public PhaseTurnCtl phaseTurn;
-  public RangeCtl rangeCtl;
-  public CCSkillRender HostSkillRender,
-  DuelSkillRender;
-  public CCPhaseRender HostPhaseRender,
-  DuelPhaseRender;
-
-  // Room Service Conn
-  public RoomServiceConn RoomConn;
 
   // service-connecter related
   public CfServiceClientSetting GDServiceConfig;
@@ -34,62 +24,14 @@ public class GDConnControl : Singleton<GDConnControl> {
 
   // monitered data
   public GameDataSet CurrentGS;
-  private bool isInited = false;
-  public bool UpdateFlag;
+  private bool isInited = false, UpdateFlag;
+  public string RoomKey;
   // Life-cycle of the process
-  async void Start() {
-    if (this.cCardResx == null)this.cCardResx = GameObject.Find("EventSystem").GetComponent<CCardSetUp>();
-    if (this.phaseTurn == null)this.phaseTurn = GameObject.Find("VisualEffectLayer/phase_turn").GetComponent<PhaseTurnCtl>();
-    if (this.rangeCtl == null)this.rangeCtl = GameObject.Find("StandLayer").GetComponent<RangeCtl>();
-    if (this.HostSkillRender == null)this.HostSkillRender = GameObject.Find("SkillLayer/SelfSkillRender").GetComponent<CCSkillRender>();
-    if (this.DuelSkillRender == null)this.DuelSkillRender = GameObject.Find("SkillLayer/DuelSkillRender").GetComponent<CCSkillRender>();
-    if (this.HostPhaseRender == null)this.HostPhaseRender = GameObject.Find("PhaseLayer/selfphase").GetComponent<CCPhaseRender>();
-    if (this.DuelPhaseRender == null)this.DuelPhaseRender = GameObject.Find("PhaseLayer/duelphase").GetComponent<CCPhaseRender>();
 
-    if (!isInited)
-      isInited = await FullSetup();
-    // StartCoroutine( InitGameCtlSetup());
-  }
 
-  protected virtual void Update() {
-    // if (UpdateFlag) {
-    //   UpdateFlag = false;
-    // }
-  }
-  async Task<bool> FullSetup() {
-    Debug.Log("MyScript.Start " + GetInstanceID(), this);
-    var objs = GameObject.FindGameObjectWithTag("room_connector");
-    var v = objs.GetComponent<RoomServiceConn>();
-    this.RoomConn = v;
-    this.InitConnSetup(this.RoomConn.config);
-    // yield return true;
-    this.InitGameCtlSetup();
 
-    if (RoomConn.IsHost && !RoomConn.IsWatcher) {
-      Debug.Log("Host-Create");
-      CharCardSet hostPack = new CharCardSet();
-      CharCardSet duelPack = new CharCardSet();
-      List<CharCardSet> hostPackList = new List<CharCardSet>();
-      List<CharCardSet> duelPackList = new List<CharCardSet>();
-      if (this.cCardResx.SelfDataCardSet != null) {
-        hostPack = this.cCardResx.SelfDataCardSet.ToCharCardSetRaw();
-      }
-      if (this.cCardResx.DuelDataCardSet != null) {
-        duelPack = this.cCardResx.DuelDataCardSet.ToCharCardSetRaw();
-      }
-      hostPackList.Add(hostPack);
-      duelPackList.Add(duelPack);
-
-      await this.CreateGameSet(
-        this.RoomConn.CurrentRoom.CharCardNvn,
-        hostPackList, duelPackList
-      );
-      // this.SendMsg("Host:CreatedGame,GetGameSet");
-    }
-    return true;
-  }
   // Custom Code 
-  void InitConnSetup(CfServerSetting setting) {
+  public void InitConnSetup(CfServerSetting setting, string RoomKey) {
     Debug.Log(setting);
     this.GDServiceConfig = setting.GameDuelService;
     this.main_ch = new Channel(
@@ -103,108 +45,45 @@ public class GDConnControl : Singleton<GDConnControl> {
     this.natOpt = ConnectionFactory.GetDefaultOptions();
     this.natOpt.Url = $"{streamSet.Connector}://{streamSet.Host}:{streamSet.Port}";
     Debug.Log(this.natOpt.Url);
+    this.RoomKey = RoomKey;
     this.natConn = new ConnectionFactory().CreateConnection(this.natOpt);
-    this.natConn.SubscribeAsync($"ULZ.GDSvc/{this.RoomConn.CurrentRoom.Key}", this.OnSubMsgHandle);
+    // this.natConn.SubscribeAsync($"ULZ.GDSvc/{RoomKey}", this.OnSubMsgHandle);
   }
 
-  void InitGameCtlSetup() {
-    Debug.Log("InitGameCtlSetup");
-    if (this.RoomConn == null || this.RoomConn.CurrentRoom == null) {
-      return;
-    }
-
-    if (!this.RoomConn.IsHost) {
-
-      var phase_layer = GameObject.FindGameObjectWithTag("phase_layer").transform;
-
-      this.HostPhaseRender =
-        phase_layer.Find("duelphase").GetComponent<CCPhaseRender>();
-      this.DuelPhaseRender =
-        phase_layer.Find("selfphase").GetComponent<CCPhaseRender>();
-
-      var skill_layer = GameObject.FindGameObjectWithTag("skill_layer").transform;
-
-      this.HostSkillRender =
-        skill_layer.Find("DuelSkillRender").GetComponent<CCSkillRender>();
-      this.DuelSkillRender =
-        skill_layer.Find("SelfSkillRender").GetComponent<CCSkillRender>();
-
-      this.cCardResx.DuelCC_ID = this.RoomConn.CurrentRoom.HostCharcardId;
-      this.cCardResx.DuelCC_Level = this.RoomConn.CurrentRoom.HostCardlevel;
-      this.cCardResx.DuelCardSet_ID = this.RoomConn.CurrentRoom.HostCardsetId;
-
-      this.cCardResx.SelfCC_ID = this.RoomConn.CurrentRoom.DuelCharcardId;
-      this.cCardResx.SelfCC_Level = this.RoomConn.CurrentRoom.DuelCardlevel;
-      this.cCardResx.SelfCardSet_ID = this.RoomConn.CurrentRoom.DuelCardsetId;
-
-    } else {
-      this.cCardResx.SelfCC_ID = this.RoomConn.CurrentRoom.HostCharcardId;
-      this.cCardResx.SelfCC_Level = this.RoomConn.CurrentRoom.HostCardlevel;
-      this.cCardResx.SelfCardSet_ID = this.RoomConn.CurrentRoom.HostCardsetId;
-
-      this.cCardResx.DuelCC_ID = this.RoomConn.CurrentRoom.DuelCharcardId;
-      this.cCardResx.DuelCC_Level = this.RoomConn.CurrentRoom.DuelCardlevel;
-      this.cCardResx.DuelCardSet_ID = this.RoomConn.CurrentRoom.DuelCardsetId;
-    }
-
-    StartCoroutine(this.cCardResx.StartResxLoad());
-
-    StartCoroutine(this.cCardResx.SelfCCImplement());
-    StartCoroutine(this.cCardResx.DuelCCImplement());
-  }
-
-  async void OnSubMsgHandle(object caller, NATS.Client.MsgHandlerEventArgs income) {
-    Debug.Log(caller);
-    var inst_msg = GDBroadcastResp.Parser.ParseFrom(income.Message.Data);
-    Debug.Log(inst_msg);
-    Debug.Log(inst_msg.Command.ToString());
-    switch (inst_msg.Command) {
-      case CastCmd.GetGamesetResult:
-        Debug.Log(inst_msg.Msg);
-        // GameDataSet task;
-        if (inst_msg.Msg == "Host:CreatedGame,GetGameSet" && (this.RoomConn.IsWatcher || !this.RoomConn.IsHost)) {
-          this.GetGameData(this.RoomConn.CurrentRoom.Key, this.RoomConn.IsWatcher).Wait();
-        }
-        Debug.Log(this.CurrentGS.HostCardDeck);
-        Debug.Log(this.CurrentGS.DuelCardDeck);
-        break;
-      case CastCmd.GetEffectResult:
-      case CastCmd.GetDrawPhaseResult:
-      case CastCmd.GetMovePhaseResult:
-      case CastCmd.GetAtkPhaseResult:
-      case CastCmd.GetDefPhaseResult:
-
-      case CastCmd.InstanceDamage:
-      case CastCmd.InstanceStatusChange:
-        break;
-    }
-  }
-  bool SendMsg(string msg) {
-    if (this.natConn == null)throw new System.Exception("Null_Conn");
-    if (this.RoomConn.CurrentRoom == null || this.RoomConn.CurrentRoom.Key == "")throw new System.Exception("No_Current_Room");
-
-    var msgPack = new GDBroadcastResp {
-      RoomKey = this.RoomConn.CurrentRoom.Key,
-      Msg = msg,
-      Command = CastCmd.GetGamesetResult,
-      Side = this.RoomConn.IsHost? PlayerSide.Host : PlayerSide.Dueler,
-    };
-
-    this.natConn.Publish($"ULZ.GDSvc/{this.RoomConn.CurrentRoom.Key}", msgPack.ToByteArray());
+  public bool AddSubMsgHandle(System.EventHandler<MsgHandlerEventArgs> handler) {
+    if (this.natConn == null)return false;
+    this.natConn.SubscribeAsync($"ULZ.GDSvc/{RoomKey}", handler);
     return true;
   }
 
-  public async Task<GameDataSet> CreateGameSet(int nvn,
+  public bool SendMsg(string msg, bool IsHost) {
+    if (this.natConn == null)throw new System.Exception("Null_Conn");
+    if (this.RoomKey == null)throw new System.Exception("No_Current_Room");
+
+    var msgPack = new GDBroadcastResp {
+      RoomKey = this.RoomKey,
+      Msg = msg,
+      Command = CastCmd.GetGamesetResult,
+      Side = IsHost? PlayerSide.Host : PlayerSide.Dueler,
+    };
+
+    this.natConn.Publish($"ULZ.GDSvc/{this.RoomKey}", msgPack.ToByteArray());
+    return true;
+  }
+
+  public async Task<GameDataSet> CreateGameSet(
+    int nvn, string HostId, string DuelId,
     List<CharCardSet> HostCardList = null, List<CharCardSet> DuelerCardList = null,
     List<EventCard> HostECList = null, List<EventCard> DuelerECList = null) {
-    if (this.RoomConn.CurrentRoom == null)throw new System.Exception("NO_ROOM_EXIST");
+    // if (this.RoomConn.CurrentRoom == null)throw new System.Exception("NO_ROOM_EXIST");
+    if (this.RoomKey == null)throw new System.Exception("NO_ROOM_KEY");
     if (this.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
 
     try {
       var req = new GDCreateReq {
-        RoomKey = this.RoomConn.CurrentRoom.Key,
-        HostId = this.RoomConn.CurrentRoom.Host.Id,
-        DuelerId = this.RoomConn.CurrentRoom.Dueler.Id,
+        RoomKey = RoomKey,
+        HostId = HostId,
+        DuelerId = DuelId,
         Nvn = nvn,
       };
 
@@ -222,12 +101,12 @@ public class GDConnControl : Singleton<GDConnControl> {
     }
   }
   public async Task<GameDataSet> GetGameData(string roomKey, bool isWatcher) {
-    if (this.RoomConn.CurrentRoom == null)throw new System.Exception("NO_ROOM_EXIST");
     if (this.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
+    // if (this.RoomKey == null || this.RoomKey == "")throw new System.Exception("NO_ROOM_KEY");
 
     try {
       var req = new GDGetInfoReq {
-        RoomKey = this.RoomConn.CurrentRoom.Key,
+        RoomKey = roomKey == null ? this.RoomKey : roomKey,
         IsWatcher = isWatcher,
       };
       var tmpGameSet = await this.GDSClient.GetGameDataAsync(req);
@@ -253,14 +132,14 @@ public class GDConnControl : Singleton<GDConnControl> {
     }
   }
 
-  public async Task<bool> InstSetEventCard(List<EventCard> eclist) {
+  public async Task<bool> InstSetEventCard(List<EventCard> eclist, bool isHost) {
     if (this.CurrentGS == null)throw new System.Exception("NO_ROOM_EXIST");
     if (this.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
 
     try {
       var req = new GDInstanceDT {
         RoomKey = CurrentGS.RoomKey,
-        Side = this.RoomConn.IsHost? PlayerSide.Host : PlayerSide.Dueler,
+        Side = isHost? PlayerSide.Host : PlayerSide.Dueler,
         CurrentPhase = this.CurrentGS.EventPhase,
       };
       foreach (var ec in eclist) {
---
+++ Assets/Scripts/CardPlay/MainCtrlComp.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Grpc.Core;
+using NATS.Client;
+using ULZAsset.Config;
+using ULZAsset.MsgExtension;
+using ULZAsset.ProtoMod.GameDuelService;
+using ULZAsset.ProtoMod.RoomService;
+using UnityEngine;
+
+public class MainCtrlComp : MonoBehaviour {
+    // Start is called before the first frame update
+    public CCardSetUp cCardResx;
+
+    public PhaseTurnCtl phaseTurn;
+    public RangeCtl rangeCtl;
+    public CCSkillRender HostSkillRender, DuelSkillRender;
+    public CCPhaseRender HostPhaseRender, DuelPhaseRender;
+
+    // Room Service Conn
+    public GDConnControl GDConn;
+    public RoomServiceConn RoomConn;
+    async void Start() {
+        await FullSetup();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    void InitGameCtlSetup() {
+        Debug.Log("InitGameCtlSetup");
+        if (this.RoomConn == null || this.RoomConn.CurrentRoom == null) {
+            return;
+        }
+
+        if (!this.RoomConn.IsHost) {
+
+            var phase_layer = GameObject.FindGameObjectWithTag("phase_layer").transform;
+
+            this.HostPhaseRender =
+                phase_layer.Find("duelphase").GetComponent<CCPhaseRender>();
+            this.DuelPhaseRender =
+                phase_layer.Find("selfphase").GetComponent<CCPhaseRender>();
+
+            var skill_layer = GameObject.FindGameObjectWithTag("skill_layer").transform;
+
+            this.HostSkillRender =
+                skill_layer.Find("DuelSkillRender").GetComponent<CCSkillRender>();
+            this.DuelSkillRender =
+                skill_layer.Find("SelfSkillRender").GetComponent<CCSkillRender>();
+
+            this.cCardResx.DuelCC_ID = this.RoomConn.CurrentRoom.HostCharcardId;
+            this.cCardResx.DuelCC_Level = this.RoomConn.CurrentRoom.HostCardlevel;
+            this.cCardResx.DuelCardSet_ID = this.RoomConn.CurrentRoom.HostCardsetId;
+
+            this.cCardResx.SelfCC_ID = this.RoomConn.CurrentRoom.DuelCharcardId;
+            this.cCardResx.SelfCC_Level = this.RoomConn.CurrentRoom.DuelCardlevel;
+            this.cCardResx.SelfCardSet_ID = this.RoomConn.CurrentRoom.DuelCardsetId;
+
+        } else {
+            this.cCardResx.SelfCC_ID = this.RoomConn.CurrentRoom.HostCharcardId;
+            this.cCardResx.SelfCC_Level = this.RoomConn.CurrentRoom.HostCardlevel;
+            this.cCardResx.SelfCardSet_ID = this.RoomConn.CurrentRoom.HostCardsetId;
+
+            this.cCardResx.DuelCC_ID = this.RoomConn.CurrentRoom.DuelCharcardId;
+            this.cCardResx.DuelCC_Level = this.RoomConn.CurrentRoom.DuelCardlevel;
+            this.cCardResx.DuelCardSet_ID = this.RoomConn.CurrentRoom.DuelCardsetId;
+        }
+
+        StartCoroutine(this.cCardResx.StartResxLoad());
+
+        StartCoroutine(this.cCardResx.SelfCCImplement());
+        StartCoroutine(this.cCardResx.DuelCCImplement());
+    }
+    async Task<bool> FullSetup() {
+        Debug.Log("MyScript.Start " + GetInstanceID(), this);
+        var objs = GameObject.FindGameObjectWithTag("room_connector");
+        var v = objs.GetComponent<RoomServiceConn>();
+        this.RoomConn = v;
+        this.GDConn.InitConnSetup(this.RoomConn.config, this.RoomConn.CurrentRoom.Key);
+        this.GDConn.AddSubMsgHandle(OnSubMsgHandle);
+        // yield return true;
+        this.InitGameCtlSetup();
+
+        if (RoomConn.IsHost && !RoomConn.IsWatcher) {
+            Debug.Log("Host-Create");
+            CharCardSet hostPack = new CharCardSet();
+            CharCardSet duelPack = new CharCardSet();
+            List<CharCardSet> hostPackList = new List<CharCardSet>();
+            List<CharCardSet> duelPackList = new List<CharCardSet>();
+            if (this.cCardResx.SelfDataCardSet != null) {
+                hostPack = this.cCardResx.SelfDataCardSet.ToCharCardSetRaw();
+            }
+            if (this.cCardResx.DuelDataCardSet != null) {
+                duelPack = this.cCardResx.DuelDataCardSet.ToCharCardSetRaw();
+            }
+            hostPackList.Add(hostPack);
+            duelPackList.Add(duelPack);
+
+            await this.GDConn.CreateGameSet(
+                this.RoomConn.CurrentRoom.CharCardNvn,
+                this.RoomConn.CurrentRoom.Host.Id, this.RoomConn.CurrentRoom.Dueler.Id,
+                hostPackList, duelPackList
+            );
+            // this.SendMsg("Host:CreatedGame,GetGameSet");
+        }
+        return true;
+    }
+
+    async void OnSubMsgHandle(object caller, NATS.Client.MsgHandlerEventArgs income) {
+        Debug.Log(caller);
+        var inst_msg = GDBroadcastResp.Parser.ParseFrom(income.Message.Data);
+        Debug.Log(inst_msg);
+        Debug.Log(inst_msg.Command.ToString());
+        switch (inst_msg.Command) {
+            case CastCmd.GetGamesetResult:
+                Debug.Log(inst_msg.Msg);
+                // GameDataSet task;
+                if (inst_msg.Msg == "Host:CreatedGame,GetGameSet" && (this.RoomConn.IsWatcher || !this.RoomConn.IsHost)) {
+                    await this.GDConn.GetGameData(this.RoomConn.CurrentRoom.Key, this.RoomConn.IsWatcher);
+                }
+                Debug.Log(this.GDConn.CurrentGS.HostCardDeck);
+                Debug.Log(this.GDConn.CurrentGS.DuelCardDeck);
+                break;
+            case CastCmd.GetEffectResult:
+            case CastCmd.GetDrawPhaseResult:
+            case CastCmd.GetMovePhaseResult:
+            case CastCmd.GetAtkPhaseResult:
+            case CastCmd.GetDefPhaseResult:
+
+            case CastCmd.InstanceDamage:
+            case CastCmd.InstanceStatusChange:
+                break;
+        }
+    }
+}

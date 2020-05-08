cb139d5e52d1aabb21d8d0266332d06ee5eef93f
steven <stv.chm@gmail.com>
2020-05-08 18:37:36 +0800

    fix

--- Assets/Scripts/CardPlay/Connecter/GDConnControl.cs
+++ Assets/Scripts/CardPlay/Connecter/GDConnControl.cs
@@ -32,41 +32,41 @@ public class GDConnControl : Singleton<GDConnControl> {
   // Custom Code 
   public void InitConnSetup(CfServerSetting setting, string RoomKey) {
     Debug.Log(setting);
-    this.GDServiceConfig = setting.GameDuelService;
-    this.main_ch = new Channel(
+    instance.GDServiceConfig = setting.GameDuelService;
+    instance.main_ch = new Channel(
       setting.GameDuelService.Host, setting.GameDuelService.Port,
       ChannelCredentials.Insecure
     );
 
-    this.GDSClient = new GameDuelService.GameDuelServiceClient(this.main_ch);
+    instance.GDSClient = new GameDuelService.GameDuelServiceClient(instance.main_ch);
 
     var streamSet = setting.GameDuelService.StreamSetting;
-    this.natOpt = ConnectionFactory.GetDefaultOptions();
-    this.natOpt.Url = $"{streamSet.Connector}://{streamSet.Host}:{streamSet.Port}";
-    Debug.Log(this.natOpt.Url);
-    this.RoomKey = RoomKey;
-    this.natConn = new ConnectionFactory().CreateConnection(this.natOpt);
-    // this.natConn.SubscribeAsync($"ULZ.GDSvc/{RoomKey}", this.OnSubMsgHandle);
+    instance.natOpt = ConnectionFactory.GetDefaultOptions();
+    instance.natOpt.Url = $"{streamSet.Connector}://{streamSet.Host}:{streamSet.Port}";
+    Debug.Log(instance.natOpt.Url);
+    instance.RoomKey = RoomKey;
+    instance.natConn = new ConnectionFactory().CreateConnection(instance.natOpt);
+    // instance.natConn.SubscribeAsync($"ULZ.GDSvc/{RoomKey}", instance.OnSubMsgHandle);
   }
 
   public bool AddSubMsgHandle(System.EventHandler<MsgHandlerEventArgs> handler) {
-    if (this.natConn == null)return false;
-    this.natConn.SubscribeAsync($"ULZ.GDSvc/{RoomKey}", handler);
+    if (instance.natConn == null)return false;
+    instance.natConn.SubscribeAsync($"ULZ.GDSvc/{RoomKey}", handler);
     return true;
   }
 
   public bool SendMsg(string msg, bool IsHost) {
-    if (this.natConn == null)throw new System.Exception("Null_Conn");
-    if (this.RoomKey == null)throw new System.Exception("No_Current_Room");
+    if (instance.natConn == null)throw new System.Exception("Null_Conn");
+    if (instance.RoomKey == null)throw new System.Exception("No_Current_Room");
 
     var msgPack = new GDBroadcastResp {
-      RoomKey = this.RoomKey,
+      RoomKey = instance.RoomKey,
       Msg = msg,
       Command = CastCmd.GetGamesetResult,
       Side = IsHost? PlayerSide.Host : PlayerSide.Dueler,
     };
 
-    this.natConn.Publish($"ULZ.GDSvc/{this.RoomKey}", msgPack.ToByteArray());
+    instance.natConn.Publish($"ULZ.GDSvc/{instance.RoomKey}", msgPack.ToByteArray());
     return true;
   }
 
@@ -74,10 +74,10 @@ public class GDConnControl : Singleton<GDConnControl> {
     int nvn, string HostId, string DuelId,
     List<CharCardSet> HostCardList = null, List<CharCardSet> DuelerCardList = null,
     List<EventCard> HostECList = null, List<EventCard> DuelerECList = null) {
-    // if (this.RoomConn.CurrentRoom == null)throw new System.Exception("NO_ROOM_EXIST");
-    if (this.RoomKey == null)throw new System.Exception("NO_ROOM_KEY");
-    if (this.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
-    if (this.CurrentGS != null)return this.CurrentGS;
+    // if (instance.RoomConn.CurrentRoom == null)throw new System.Exception("NO_ROOM_EXIST");
+    if (instance.RoomKey == null)throw new System.Exception("NO_ROOM_KEY");
+    if (instance.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
+    if (instance.CurrentGS != null)return instance.CurrentGS;
     try {
       var req = new GDCreateReq {
         RoomKey = RoomKey,
@@ -91,40 +91,40 @@ public class GDConnControl : Singleton<GDConnControl> {
       if (HostECList != null)req.HostExtraEc.AddRange(HostECList);
       if (DuelerECList != null)req.DuelExtraEc.AddRange(DuelerECList);
 
-      var tmpGameSet = await this.GDSClient.CreateGameAsync(req);
+      var tmpGameSet = await instance.GDSClient.CreateGameAsync(req);
       Debug.Log($"{tmpGameSet.CurrPhase}");
-      this.CurrentGS = tmpGameSet;
-      return this.CurrentGS;
+      instance.CurrentGS = tmpGameSet;
+      return instance.CurrentGS;
     } catch (RpcException) {
       throw;
     }
   }
   public async Task<GameDataSet> GetGameData(string roomKey, bool isWatcher) {
-    if (this.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
-    // if (this.RoomKey == null || this.RoomKey == "")throw new System.Exception("NO_ROOM_KEY");
+    if (instance.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
+    // if (instance.RoomKey == null || instance.RoomKey == "")throw new System.Exception("NO_ROOM_KEY");
 
     try {
       var req = new GDGetInfoReq {
-        RoomKey = roomKey == null ? this.RoomKey : roomKey,
+        RoomKey = roomKey == null ? instance.RoomKey : roomKey,
         IsWatcher = isWatcher,
       };
-      var tmpGameSet = await this.GDSClient.GetGameDataAsync(req);
-      this.CurrentGS = tmpGameSet;
-      return this.CurrentGS;
+      var tmpGameSet = await instance.GDSClient.GetGameDataAsync(req);
+      instance.CurrentGS = tmpGameSet;
+      return instance.CurrentGS;
     } catch (RpcException) {
       throw;
     }
   }
 
   public async Task<bool> QuitGame() {
-    if (this.CurrentGS == null)throw new System.Exception("NO_ROOM_EXIST");
-    if (this.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
+    if (instance.CurrentGS == null)throw new System.Exception("NO_ROOM_EXIST");
+    if (instance.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
 
     try {
       var req = new GDCreateReq {
         RoomKey = CurrentGS.RoomKey,
       };
-      var tmpGameSet = await this.GDSClient.QuitGameAsync(req);
+      var tmpGameSet = await instance.GDSClient.QuitGameAsync(req);
       return true;
     } catch (RpcException) {
       throw;
@@ -132,19 +132,19 @@ public class GDConnControl : Singleton<GDConnControl> {
   }
 
   public async Task<bool> InstSetEventCard(List<EventCard> eclist, bool isHost) {
-    if (this.CurrentGS == null)throw new System.Exception("NO_ROOM_EXIST");
-    if (this.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
+    if (instance.CurrentGS == null)throw new System.Exception("NO_ROOM_EXIST");
+    if (instance.GDSClient == null)throw new System.Exception("NO_CLIENT_EXIST");
 
     try {
       var req = new GDInstanceDT {
         RoomKey = CurrentGS.RoomKey,
         Side = isHost? PlayerSide.Host : PlayerSide.Dueler,
-        CurrentPhase = this.CurrentGS.EventPhase,
+        CurrentPhase = instance.CurrentGS.EventPhase,
       };
       foreach (var ec in eclist) {
         req.UpdateCard.Add(GDMsg.ConvertToEC(ec));
       }
-      await this.GDSClient.InstSetEventCardAsync(req);
+      await instance.GDSClient.InstSetEventCardAsync(req);
       return true;
     } catch (RpcException) {
       throw;
